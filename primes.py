2834b0934a57794697651e1b2c8df9c3f6cce5d2
Paul Cheung <cheungkuensum@gmail.com>
2022-09-30 11:05:17 +0100

    Add code to return prime numbers as required

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    count =0
+    current_num = 2
+    isPrime = True
+    while count<number_of_primes:
+    	for i in range(2,current_num):
+    		if current_num%i ==0:
+    			isPrime = False
+    			break
+    	if isPrime:
+    		list.append(current_num)
+    		count+=1
+    	current_num += 1
+    	isPrime = True
     return list
